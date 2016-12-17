8feb8dd4f306defdf23fab6341936f3400b76fef
Jhenina Sta. Cruz <jheninasta.cruz@gmail.com>
2016-12-17 12:37:19 +0800

    index.php
    edited the delete form in index.php

--- excercise6/codeigniter/application/views/index.php
+++ excercise6/codeigniter/application/views/index.php
@@ -22,41 +22,102 @@ function show_confirm(act, gotoid){
 </script>
 <style>
 
-body{
+.body{
 
   background-image: url("gray.gif");
   opacity: 1.5;
 }
-table{
-    background-color: #EEE7DA;
-    box-shadow: 1px 1px 20px #272532;
-    border-radius: 5px;
-    font-family: helvetica;
-    width:90%;
-    color:#6F5F5C;
-    margin-bottom:80px;
-}
-table a{
- text-decoration:none;
- color:#5B5552;
-}
-table,td,th{
- border-collapse:collapse;
- border:solid #d0d0d0 1px;
- padding:20px;
-}
-table td input{
- width:97%;
- height:35px;
- border:dashed #00a2d1 1px;
- padding-left:15px;
- font-family:Verdana, Geneva, sans-serif;
- box-shadow:0px 0px 0px rgba(1,0,0,0.2);
- outline:none;
-}
-#add_data:hover{
-    background-color: #d6cfc4;
-}
+    table{
+        background-color: #EEE7DA;
+        box-shadow: 1px 1px 20px #272532;
+        border-radius: 5px;
+        font-family: helvetica;
+        width:90%;
+        color:#6F5F5C;
+        margin-bottom:80px;
+    }
+    table a{
+     text-decoration:none;
+     color:#5B5552;
+    }
+    table,td,th{
+     border-collapse:collapse;
+     border:solid #d0d0d0 1px;
+     padding:20px;
+    }
+    table td input{
+     width:97%;
+     height:35px;
+     border:dashed #00a2d1 1px;
+     padding-left:15px;
+     font-family:Verdana, Geneva, sans-serif;
+     box-shadow:0px 0px 0px rgba(1,0,0,0.2);
+     outline:none;
+    }
+    #add_data:hover{
+        background-color: #d6cfc4;
+    }
+    .box1{
+      position: absolute;
+      left: 30%;
+      margin-top: 20px;
+      background-color: #EEE7DA;
+      box-shadow: 1px 2px 20px #272532;
+      border-radius: 5px;
+      font-family: calibri;
+      width: 500px;
+      height: 600px;
+    }
+    .error {color: #FF0000;}
+    h2{
+      text-align: center;
+    }
+    textarea{
+      width: 200px;
+      height: 50px;
+      box-sizing: border-box;
+      border: 2px solid #ccc;
+      border-radius: 4px;
+      font-size: 16px;
+      background-color: white;
+      background-position: 10px 10px;
+      background-repeat: no-repeat;
+      padding: 12px 20px 12px 40px;
+    }
+    .content{
+      padding-left: 90px;
+      padding-right: 60px;
+      color: #5B5552;
+    }
+    p{
+      padding-left: 40px;
+      padding-right: 60px;
+      color: #5B5552;
+    }
+    #formValid{
+      color: #5B5552;
+    }
+    #hide{
+      display: none;
+    }
+    input{
+      margin-left: 25px;
+    }
+    #submit{
+      position: absolute;
+      margin-left: 210px;
+    }
+    input[type=text], select {
+      width: 200px;
+      height: 35px;
+      padding: 12px 15px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-sizing: border-box
+    }
+
 </style>
 </head>
 <body>
@@ -91,7 +152,7 @@ table td input{
         <td><?php echo $u_key->comment; ?></td>
         <td><?php echo $u_key->gender; ?></td>
   
-		 <td align="center"><a href="<?php echo base_url() . "index.php/users/show_users_id/" . $u_key->user_id; ?>" onClick="show_confirm('edit',<?php echo $u_key->user_id;?>)"><img src="<?php echo base_url();?>/images/b_edit.png" style="width: 20px"></img></a></td>
+		 <td align="center"><a href="<?php echo base_url() . "index.php/users/show_users_id/" . $u_key->user_id; ?>" onClick="show_confirm('edit',<?php echo $u_key->user_id;?>)"><img src="<?php echo base_url();?>/images/b_edit.png" style="width: 30px"></img></a></td>
         
 
         <td align="center"><a href="#" onClick="show_confirm('delete',<?php echo $u_key->user_id;?>)"><img src="<?php echo base_url();?>/images/b_drop.png"></img></a></td>
