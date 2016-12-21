235959969ef5fb4e34ef6394b76b5c91e95a52e8
Jhenina Sta. Cruz <jheninasta.cruz@gmail.com>
2016-12-22 00:41:56 +0800

    tweaked SiteController.php in trivias

--- excercise7/controllers/SiteController.php
+++ excercise7/controllers/SiteController.php
@@ -8,6 +8,8 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\UserFormModel;
+use app\models\Trivias;
 
 class SiteController extends Controller
 {
@@ -60,7 +62,14 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+         $query = Trivias::find()
+        ->orderBy(new Expression('rand()'))
+        ->limit(5)
+        ->all();
+
+        return $this->render('index', [
+        'trivias' => $query,
+        ]);
     }
 
     /**
