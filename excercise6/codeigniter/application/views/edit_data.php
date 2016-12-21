686d8705d7588c70de085c431d4021747622ceeb
Jhenina Sta. Cruz <jheninasta.cruz@gmail.com>
2016-12-21 22:38:43 +0800

    edited template
    edited edit_data.php css template and added a line to make function work

--- excercise6/codeigniter/application/views/edit_data.php
+++ excercise6/codeigniter/application/views/edit_data.php
@@ -3,91 +3,189 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>Form Validation: Edit Data </title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
 <style>
+body {
+      background-color: #FCEDEA;
+    }
+    h1 {
+      color: black;
+      text-align: center;
+    }
+    p {
+      font-family: aubrey;
+      font-size: 20px;
+    }
+    .active {
+      background-color:transparent;
+    }
+    .city {
+       padding-left: 100px;
+       padding-right: 300px;
+       padding-top: 60px;
+        position: relative;
+        float: center;
+        background-color: #FFE1CA;
+        box-shadow: 1px 2px 20px #272532;
+        border-radius: 5px;
+        font-family: aubrey;
+        width: 500px;
+        height: 750px;
 
-body{
-  background-image: url("gray.gif");
-  opacity: 1.5;
-}
-.box{
-  position: absolute;
-  left: 30%;
-  margin-top: 20px;
-  background-color: #EEE7DA;
-  
-  border-radius: 5px;
-  font-family: helvetica;
-  width: 500px;
-  height: 600px;
-}
-.error {color: #FF0000;}
-h2{
-  text-align: center;
-}
-textarea{
-  width: 200px;
-  height: 50px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  background-color: white;
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  padding: 12px 20px 12px 40px;
-}
-p, form{
-  padding-left: 50px;
-  padding-right: 60px;
-  color: #5B5552;
-}
-#formValid{
-  color: #5B5552;
-}
-input{
-  margin-left: 25px;
-}
-#submit{
-  position: absolute;
-  margin-left: 210px;
-}
-input[type=text], select {
-  width: 200px;
-  height: 35px;
-  padding: 12px 15px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-sizing: border-box;
-}
+      }
+    a:link, a:visited {
+      background-color: #FFE1CA;
+      color: black;
+      padding: 30px 40px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-family: aubrey;
+      font-size: 20px;
 
+    }
+    a:hover, a:active {
+      background-color: black;
+      color: white;
+    }
+    p{
+      padding-right: 200px;
+      padding-left: 200px;
+    }
+    #footer {
+      position:fixed;
+      left:0px; 
+      bottom:0px;
+      height:30px;
+      width:100%;
+      background:#999;
+
+    }
+    .form {
+      padding-right: 190px;
+      padding-left: 200px;
+      text-align: center;
+        position: absolute;
+        float: center;
+        margin-top: 20px;
+        background-color: #FFE1CA;
+        box-shadow: 1px 2px 20px #272532;
+        border-radius: 5px;
+        font-family: georgia;
+        width: 500px;
+        height: 600px;
   }
+  .error {color: #FF0000;}
+    
+      h2{
+    text-align: center;
+  }
+    textarea{
+      width: 300px;
+      height: 70px;
+      box-sizing: border-box;
+      border: 2px solid #ccc;
+      border-radius: 4px;
+      font-size: 18px;
+      background-color: white;
+      padding: 12px 20px 12px 40px;
+}
+        
+    p{
+      padding-left: 40px;
+      padding-right: 60px;
+      color: #5B5552;
+    }
+    #formValid{
+      color: #5B5552;
+    }
+    #hide{
+      display: none;
+    }
+    input{
+      margin-left: 25px;
+    }
+    #submit{
+      width: 200px;
+      height: 40px;
+      position: absolute;
+      margin-left: 210px;
+    }
+    input[type=text], select {
+      width: 300px;
+      height: 50px;
+      padding: 12px 15px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-sizing: border-box;
+    }
+    .para2_white{
+      position: absolute;
+      font-family: georgia;
+      font-size: 14px;
+      color: #EEE7DA;
+      line-height: 0.5em;
+     }
+     .image1{
+      z-index: -0.1px;
+     }
+   }
+
+</style>
+    <head>
+    <body background="<?php echo base_url();?>/images/bg2.png" style="opacity: 1.5;">
+      <center>
+      <div style="padding-left:445px; padding-right:500px;">
+      <img class="image1" src="<?php echo base_url();?>/images/girlheader.png" width="400px" height="563px" style="padding-top:1%;position:absolute;top:0;align:center">
+        <img class="image2" src="<?php echo base_url();?>/images/finalheader.png" width="720px" height="140px" style=" padding-top:70%; position:relative; top:50">
+
+</div>
+  
+  <br>
+  <br>
+  
+  <body>
+    <center>
+      <a href="<?php echo base_url('index.php/AboutMe/index')?>"> About Me</a>
+      <a href="<?php echo base_url('index.php/MyPassion/index')?>"> My Passions</a>
+      <a href="<?php echo base_url('index.php/MyHobbies/index')?>"> My Hobbies </a>
+      <a href="<?php echo base_url('index.php/MyTrivias/index')?>"> Trivias</a>
+      <a href="form.php">PHP Form</a>
+    </center>
+
+  </body>
+  </center>
+  </div>
+
+  <br>
+  <br>
+  
+  
+  <center>
+  <br>
+  <br>
 </style>
 <body>
 <center>
 
 
-<div class = "main">
- <div id="content">
+<div class = "city">
+ 
     <label>Jhenina's Form Inputs</label>
-    </div>
-</div>
 
- <div id="content">
 
  <?php foreach ($single_users as $users): ?>
     <form method="post" action="<?php echo base_url() . "index.php/users/update_users_id1"?>">
     <table align="center">
-	<td><a href = "index.php"> Back to Main Page </a></td>
-  
-  <h3 id='form_head'>Codelgniter Form Submit </h3><br/>
-<div id="form_input">
+      <tr align = "center">
 
+        <input type="text" id="hide" name="did" value="<?php echo $users->user_id; ?>">
+        
 <tr>
 	<td>
 	<input type="text" name="name" placeholder="Name" value="<?php echo $users->name; ?>" required />
